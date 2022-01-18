0914cbfee7840c16762e97e734217c6475a862fe
Gabriela <diana-gabriela.oprea@chch.ox.ac.uk>
2022-01-18 12:43:52 +0000

    Solved the orbital occupation issue

--- mom_rhf.py
+++ mom_rhf.py
@@ -14,11 +14,13 @@ import approximants as apx
 
 at_string ='H 0 0 0; H 0 0 {bond_length:.2f}'
 e_list = []
-bond_lengths = np.arange(0.2,4.6,0.05)
+#bond_lengths = np.arange(0.2,4.6,0.05)
+bond_lengths = [0.74]
+approxim = []
 for i in bond_lengths:
     mol = gto.M(
-        atom = at_string.format(bond_length = i),  # in Angstrom
-        basis = 'sto3g'
+        atom = "Be 0 0 0",  # in Angstrom
+        basis = '631g'
         #spin = 1,
     )
     myhf = scf.HF(mol)
@@ -27,63 +29,42 @@ for i in bond_lengths:
     orb = myhf.mo_coeff
     occ = myhf.mo_occ
     occ[0] = 0
-    occ[1] = 2
+    occ[5] = 2
     mo_en = myhf.mo_energy
     b = scf.HF(mol)
-    #transformation = np.array([[np.cos(np.pi/4), -np.sin(np.pi/4),0,0], [np.sin(np.pi/4), np.cos(np.pi/4),0,0],[0,0,1,0],[0,0,0,1]])
+    transformation = np.array([[np.cos(np.pi/4), -np.sin(np.pi/4),0,0], [np.sin(np.pi/4), np.cos(np.pi/4),0,0],[0,0,1,0],[0,0,0,1]])
     #transformation = np.array([[np.cos(np.pi/4), -np.sin(np.pi/4)], [np.sin(np.pi/4), np.cos(np.pi/4)]])
     #orb = orb.dot(transformation)
     dm_u = b.make_rdm1(orb, occ)
     b = scf.addons.mom_occ(b, orb, occ)
     b.scf(dm_u)
-    perm = pt.create_permutations(mol)
-    perm.reverse()
+    
         
     fock=b.get_fock()
     density=b.make_rdm1()
     hcore=b.get_hcore()
     mo_en = b.mo_energy
     orb = b.mo_coeff
-
-    for i in range(1,len(b.mo_occ)):
-        if b.mo_occ[i] == 2:
-            orb[:,[0, i]] = orb[:,[i, 0]]
+    perm = pt.create_permutations(mol, mo_en)
+    perm.reverse()
+    orb = b.mo_coeff
+    occ = b.mo_occ
+    l = 0
+    for i in range(len(occ)):
+        if occ[i] == 2:
+            orb[:,[l, i]] = orb[:,[i, l]]
+            l +=1
     fock_mo = orb.transpose().dot(fock).dot(orb)
     hcore_mo = orb.transpose().dot(hcore).dot(orb)
-    #fock = pt.full_fock(fock_mo, perm)
+    fock = pt.full_fock(fock_mo, perm)
     ao_reshaped = np.reshape(mol.intor("int2e"),(mol.nao, mol.nao, mol.nao, mol.nao))
     mo_integrals = pt.get_mo_integrals(ao_reshaped, orb)
     nuc = mol.energy_nuc()
-    #h = pt.get_full_h(hcore_mo, fock, mo_integrals, perm, nuc)
-    fock, h = pt.get_full_matrices(nuc, hcore_mo, fock_mo, mo_integrals, perm)
-    print('Htot')
-    print(h)
-    psi, e = pt.mppt(h, fock, 50)
-
+    h = pt.get_full_h(hcore_mo, fock, mo_integrals, perm, nuc)
+    psi, e = pt.mppt(h, fock, 100)
+    #r, p, q = apx.quadratic_approximant(e, 4)
+    #approxim.append(apx.get_values(r,p,q,1)[0])
     np.set_printoptions(linewidth=10000,precision=16,suppress=True)
-    e = list(e)
-    r, p, q = apx.quadratic_approximant(e, 2)
-    print(apx.get_values(r,p,q,1))
-    discriminant = p**2 - 4*q*r
-    print(discriminant.roots())
-
-    lam = np.arange(0,5,0.2)
-    l1 = []
-    l2 = []
-    el = []
-    for i in lam:
-        s = apx.get_values(r,p,q,i)
-        l1.append(s[0])
-        l2.append(s[1])
-        el.append(sum(different_lambda(e, i)))
-    plt.plot(lam,l1, color = 'b', label = 'Quadratic [2,2,2]')
-    plt.plot(lam,l2, color = 'b')
-    plt.plot(lam,el, linestyle = 'dashed', color = 'k', label ='Taylor')
-    plt.xlabel("λ")
-    plt.ylabel("E")
-    plt.legend(loc='best')
-    ax = plt.gca()
-    ax.set_ylim([-5,2])
-    plt.show()
-    e_list.append(e)
-print(e_list)
+    print(e)
+    print(np.cumsum(e))
+#print(approxim)
--- mom_uhf.py
+++ mom_uhf.py
@@ -9,7 +9,7 @@ from matplotlib import pyplot as plt
 import uhf_perturbation as pt 
 
 mol = gto.Mole()
-mol.atom = '''H 0 0 0; H 0 0 1.5'''#in Angstrom
+mol.atom = '''Be 0 0 0'''#in Angstrom
 mol.basis = 'sto3g'
 mol.charge = 0
 mol.spin = 0
@@ -18,9 +18,13 @@ myhf = scf.UHF(mol)
 myhf.scf()
 orb = myhf.mo_coeff
 occ = myhf.mo_occ
-occ[1][0]=0
-occ[1][1]=1
-
+occ[1][1] = 0
+occ[1][3] = 1
+occ[0][0] = 0
+occ[0][3] = 1
+occ[0][1] = 0
+occ[0][2] = 1
+#occ[1][3]=1
 b = scf.UHF(mol)
 dm_u = b.make_rdm1(orb, occ)
 b =  scf.addons.mom_occ(b,orb,occ)
@@ -34,21 +38,33 @@ density=b.make_rdm1()
 hcore=b.get_hcore()
 #print(hcore)
 mo_en = b.mo_energy
-perm = pt.create_permutations(mol, mo_en)
+perm = pt.create_permutations(mol, mo_en[0])
 perm.reverse()
 orb = b.mo_coeff
-orb_beta = orb[0]
-orb_beta[:,[0, 1]] = orb_beta[:,[1, 0]]
-orb_alpha = orb[1]
+occ = b.mo_occ
+occ_alpha = occ[0]
+occ_beta = occ[1]
+orb_beta = orb[1]
+orb_alpha = orb[0]
+l_alpha = 0
+l_beta = 0
+for i in range(len(occ_alpha)):
+	if occ_alpha[i] == 1:
+		orb_alpha[:,[l_alpha, i]] = orb_alpha[:,[i, l_alpha]]
+		l_alpha +=1
+	if occ_beta[i] == 1:
+		orb_beta[:,[l_beta, i]] = orb_beta[:,[i, l_beta]]
+		l_beta+=1
 fock_mo_alpha = orb_alpha.transpose().dot(fock_alpha).dot(orb_alpha)
 fock_mo_beta = orb_beta.transpose().dot(fock_beta).dot(orb_beta)
 hcore_mo_alpha = orb_alpha.transpose().dot(hcore).dot(orb_alpha)
 hcore_mo_beta = orb_beta.transpose().dot(hcore).dot(orb_beta)
-full = pt.full_fock(fock_mo_alpha, fock_mo_beta, perm)
 ao_reshaped = np.reshape(mol.intor("int2e"),(mol.nao, mol.nao, mol.nao, mol.nao))
+alphas, betas, alpha_beta = pt.get_mo_integrals(ao_reshaped, orb_alpha, orb_beta)
 nuc = mol.energy_nuc()
-h = pt.get_full_h(hcore_mo_alpha, hcore_mo_beta, full, ao_reshaped, perm, nuc, orb_alpha, orb_beta)
-#print(full_fock)
-psi, e = pt.mppt(h, full, 100)
-print(e)
+full, h = pt.get_full_matrices(nuc, hcore_mo_alpha, hcore_mo_beta, fock_mo_alpha, fock_mo_beta,alphas, betas, alpha_beta, perm)
+print(h)
+psi, e = pt.degenerate_pt_mom(h, full, 100)
+#print(e)
+#print(np.cumsum(e))
 #plot_e(e)

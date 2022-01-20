3a5a8d7c9fd19c076f2123edb69c10a510950305
Gabriela <diana-gabriela.oprea@chch.ox.ac.uk>
2022-01-20 10:04:48 +0000

    Add files via upload

--- mom_rhf.py
+++ mom_rhf.py
@@ -19,7 +19,7 @@ bond_lengths = [0.74]
 approxim = []
 for i in bond_lengths:
     mol = gto.M(
-        atom = "Be 0 0 0",  # in Angstrom
+        atom = "Ne 0 0 0",  # in Angstrom
         basis = '631g'
         #spin = 1,
     )
@@ -28,7 +28,7 @@ for i in bond_lengths:
 
     orb = myhf.mo_coeff
     occ = myhf.mo_occ
-    occ[0] = 0
+    occ[1] = 0
     occ[5] = 2
     mo_en = myhf.mo_energy
     b = scf.HF(mol)
@@ -56,11 +56,10 @@ for i in bond_lengths:
             l +=1
     fock_mo = orb.transpose().dot(fock).dot(orb)
     hcore_mo = orb.transpose().dot(hcore).dot(orb)
-    fock = pt.full_fock(fock_mo, perm)
     ao_reshaped = np.reshape(mol.intor("int2e"),(mol.nao, mol.nao, mol.nao, mol.nao))
     mo_integrals = pt.get_mo_integrals(ao_reshaped, orb)
     nuc = mol.energy_nuc()
-    h = pt.get_full_h(hcore_mo, fock, mo_integrals, perm, nuc)
+    fock, h = pt.get_full_matrices(hcore_mo, fock_mo, mo_integrals, perm, nuc)
     psi, e = pt.mppt(h, fock, 100)
     #r, p, q = apx.quadratic_approximant(e, 4)
     #approxim.append(apx.get_values(r,p,q,1)[0])
